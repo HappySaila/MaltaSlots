e4a542d153f3473dc47616f51e555f7b51ad4e0b
HappySaila <granthaldanewilson836@gmail.com>
2020-04-24 01:04:11 +0200

    Slot machine game initial commit

---
+++ Assets/Scripts/StartRandom.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartRandom : MonoBehaviour
+{
+
+    public Animation animation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RandomStart());
+    }
+
+    IEnumerator RandomStart()
+    {
+        int rand = Random.Range(0, 6);
+        yield return new WaitForSeconds(rand);
+
+        animation.Play();
+    }
+}
